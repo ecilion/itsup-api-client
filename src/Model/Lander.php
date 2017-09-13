4a8f848b3c3bcdd7de8decae5e202afc068807fa
Alex Vasic <alex@quantox.com>
2017-09-13 16:02:42 +0200

    ITSUP-615, ITSUP-616

---
+++ src/Model/Lander.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Copyright 2016 SCTR Services
+ *
+ * Distribution and reproduction are prohibited.
+ *
+ * @package     itsup-api-client
+ * @copyright   SCTR Services LLC 2016
+ * @license     No License (Proprietary)
+ */
+
+namespace Itsup\Api\Model;
+
+use Itsup\Api\Annotation\Transform;
+
+/**
+ * @author Alex VASIC <alex@quantox.com>
+ *
+ * @method int getId()
+ * @method Account getAccount()
+ * @method string getName()
+ * @method string getUrl()
+ * @method array getOffers()
+ * @method setId(int $id)
+ * @method setAccount(Account $account)
+ * @method setName(string $name)
+ * @method setUrl(string $url)
+ * @method setOffers(Offer $offer)
+ */
+class Lander extends AbstractModel
+{
+    /**
+     * @var int
+     * @Transform("int")
+     */
+    public $id;
+
+    /**
+     * @var Account
+     * @Transform("class", class="Itsup\Api\Model\Account")
+     */
+    public $account;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var array
+     * @Transform("class", class="Itsup\Api\Model\Offer", collection=true)
+     */
+    public $offers;
+}
